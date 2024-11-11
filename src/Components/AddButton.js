3cd42e6f7d9699283a741ac0df31d1368fb91eb9
martie1983 <martinarun417@gmail.com>
2024-11-11 22:10:26 +0530

    Update AddButton.js with updated icon path

--- src/Components/AddButton.js
+++ src/Components/AddButton.js
@@ -3,7 +3,7 @@ function AddButton({add}) {
 
     return (
         <div className="add-sticky-notes" onClick = {() => add()}>
-        <img alt = "Add Sticky Notes" src="./icons/add.png" />
+        <img alt="Add Sticky Notes" src={`${process.env.PUBLIC_URL}/icons/add.png`} />
       </div>
     );
 }
